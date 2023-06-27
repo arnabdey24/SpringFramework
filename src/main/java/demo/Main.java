d4efda61b9ab7c2001f8e679881098f931671f56
arnabdey24 <arnabdey665@gmail.com>
2023-06-27 19:58:15 +0600

    XML configuration

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+
+        Doctor doctor=context.getBean(Doctor.class);
+        doctor.work();
+        System.out.println(doctor.getQualification());
+        doctor.getNurse().work();
+
+        Staff nurse=(Nurse) context.getBean("nurse");
+        nurse.work();
+
+    }
+}
